cc11eca82eb37f99b426de447ce92967351f8fcb
zhangcx01 <zhangcx01@mail.jj.cn>
2020-02-09 13:26:21 +0800

    安装body-parser 和mongoose

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ const express = require('express');
 const app = express();
 const users = require('./app/router/api/users')
 const api = require('./app/router/api');
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 app.use('/api', api);
 app.listen(3000, () => console.log('app listening on port 3000'))
